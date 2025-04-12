35d23a367c1ba5493ed0c9ce5299dcd2278b8355
jayanta dey <jayantadey398@gmail.com>
2025-04-12 11:45:44 +0530

    render data inside the container app from child

--- product/src/index.js
+++ product/src/index.js
@@ -3,7 +3,7 @@ import faker from "faker";
 let products = "";
 for (let i = 0; i < 5; i++) {
   const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  products += `<div>Name: ${name}</div>`;
 }
 
 document.querySelector("#productBox").innerHTML = products;
